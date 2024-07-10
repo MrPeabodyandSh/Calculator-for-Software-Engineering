89f4775c6957f7c8f5d43a26bfa574998700595d
Xavier Baez <XEBaez@student.fullsail.edu>
2024-07-10 14:26:43 -0400

    Added new buttons with no functionality

--- Calculator for Software Engineering/Window.cpp
+++ Calculator for Software Engineering/Window.cpp
@@ -2,29 +2,97 @@
 #include "Math.h"
 
 wxBEGIN_EVENT_TABLE(Window, wxFrame)
-EVT_BUTTON(1, Window::AddClicked)
-EVT_BUTTON(2, Window::SubtractClicked)
-EVT_BUTTON(3, Window::MultiplyClicked)
-EVT_BUTTON(4, Window::DivideClicked)
-EVT_BUTTON(5, Window::EqualsClicked)
-EVT_BUTTON(6, Window::ClearClicked)
+EVT_BUTTON(11, Window::AddClicked)
+EVT_BUTTON(12, Window::SubtractClicked)
+EVT_BUTTON(13, Window::MultiplyClicked)
+EVT_BUTTON(14, Window::DivideClicked)
+EVT_BUTTON(15, Window::EqualsClicked)
+EVT_BUTTON(16, Window::ClearClicked)
 wxEND_EVENT_TABLE()
 
 Window::Window() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 200), wxSize(500, 500))
 {
-	addButton = new wxButton(this, 1, "Add (+)", wxPoint(0, 250), wxSize(100, 100));
-	subtractButton = new wxButton(this, 2, "Subtract (-)", wxPoint(0, 360), wxSize(100, 100));
-	multiplyButton = new wxButton(this, 3, "Multiply (*)", wxPoint(150, 250), wxSize(100, 100));
-	divideButton = new wxButton(this, 4, "Divide (/)", wxPoint(150, 360), wxSize(100, 100));
-	equalsButton = new wxButton(this, 5, "=", wxPoint(230, 50), wxSize(50, 50));
-	clearButton = new wxButton(this, 6, "Clear", wxPoint(315, 300), wxSize(100, 100));
+	zeroButton = new wxButton(this, 1, "0", wxPoint(218, 403), wxSize(50, 50));
+	oneButton = new wxButton(this, 2, "1", wxPoint(167, 352), wxSize(50, 50));
+	twoButton = new wxButton(this, 3, "2", wxPoint(218, 352), wxSize(50, 50));
+	threeButton = new wxButton(this, 4, "3", wxPoint(269, 352), wxSize(50, 50));
+	fourButton = new wxButton(this, 5, "4", wxPoint(167, 301), wxSize(50, 50));
+	fiveButton = new wxButton(this, 6, "5", wxPoint(218, 301), wxSize(50, 50));
+	sixButton = new wxButton(this, 7, "6", wxPoint(269, 301), wxSize(50, 50));
+	sevenButton = new wxButton(this, 8, "7", wxPoint(167, 250), wxSize(50, 50));
+	eightButton = new wxButton(this, 9, "8", wxPoint(218, 250), wxSize(50, 50));
+	nineButton = new wxButton(this, 10, "9", wxPoint(269, 250), wxSize(50, 50));
 
-	firstNumber = new wxTextCtrl(this, 7, "0", wxPoint(0, 50), wxSize(100, 20));
-	secondNumber = new wxTextCtrl(this, 8, "0", wxPoint(100, 50), wxSize(100, 20));
-	answer = new wxTextCtrl(this, 9, "0", wxPoint(300, 50), wxSize(100, 20), wxTE_READONLY);
+	addButton = new wxButton(this, 11, "+", wxPoint(320, 250), wxSize(50, 50));
+	subtractButton = new wxButton(this, 12, "-", wxPoint(320, 301), wxSize(50, 50));
+	multiplyButton = new wxButton(this, 13, "*", wxPoint(320, 352), wxSize(50, 50));
+	divideButton = new wxButton(this, 14, "/", wxPoint(320, 403), wxSize(50, 50));
+	equalsButton = new wxButton(this, 15, "=", wxPoint(371, 403), wxSize(50, 50));
+	clearButton = new wxButton(this, 16, "Clear", wxPoint(269, 199), wxSize(50, 50));
+	moduloButton = new wxButton(this, 17, "%", wxPoint(116, 250), wxSize(50, 50));
+	sinButton = new wxButton(this, 18, "sin", wxPoint(218, 199), wxSize(50, 50));
+	cosButton = new wxButton(this, 19, "cos", wxPoint(167, 199), wxSize(50, 50));
+	tanButton = new wxButton(this, 20, "tan", wxPoint(116, 199), wxSize(50, 50));
+	backspaceButton = new wxButton(this, 21, "Back", wxPoint(320, 199), wxSize(50, 50));
+	decimalButton = new wxButton(this, 22, ".", wxPoint(269, 403), wxSize(50, 50));
+	negativeButton = new wxButton(this, 23, "+/-", wxPoint(167, 403), wxSize(50, 50));
+
+	firstNumber = new wxTextCtrl(this, 24, "", wxPoint(0, 50), wxSize(100, 20));
+	secondNumber = new wxTextCtrl(this, 25, "", wxPoint(100, 50), wxSize(100, 20));
+	answer = new wxTextCtrl(this, 26, "", wxPoint(300, 50), wxSize(100, 20), wxTE_READONLY);
 
 
 
+
+}
+
+void Window::ZeroClicked(wxCommandEvent& zero)
+{
+
+}
+
+void Window::OneClicked(wxCommandEvent& one)
+{
+
+}
+
+void Window::TwoClicked(wxCommandEvent& two)
+{
+
+}
+
+void Window::ThreeClicked(wxCommandEvent& three)
+{
+
+}
+
+void Window::FourClicked(wxCommandEvent& four)
+{
+
+}
+
+void Window::FiveClicked(wxCommandEvent& five)
+{
+
+}
+
+void Window::SixClicked(wxCommandEvent& six)
+{
+
+}
+
+void Window::SevenClicked(wxCommandEvent& seven)
+{
+
+}
+
+void Window::EightClicked(wxCommandEvent& eight)
+{
+
+}
+
+void Window::NineClicked(wxCommandEvent& nine)
+{
 
 }
 
--- Calculator for Software Engineering/Window.h
+++ Calculator for Software Engineering/Window.h
@@ -3,11 +3,29 @@
 
 class Window : public wxFrame
 {
-
+	wxButton* zeroButton = nullptr;
+	wxButton* oneButton = nullptr;
+	wxButton* twoButton = nullptr;
+	wxButton* threeButton = nullptr;
+	wxButton* fourButton = nullptr;
+	wxButton* fiveButton = nullptr;
+	wxButton* sixButton = nullptr;
+	wxButton* sevenButton = nullptr;
+	wxButton* eightButton = nullptr;
+	wxButton* nineButton = nullptr;
 	wxButton* addButton = nullptr;
 	wxButton* subtractButton = nullptr;
 	wxButton* multiplyButton = nullptr;
 	wxButton* divideButton = nullptr;
+
+	wxButton* moduloButton = nullptr;
+	wxButton* sinButton = nullptr;
+	wxButton* cosButton = nullptr;             //Implement
+	wxButton* tanButton = nullptr;            //Implement
+	wxButton* backspaceButton = nullptr;         //Implement
+	wxButton* decimalButton = nullptr;
+	wxButton* negativeButton = nullptr;
+
 	wxButton* equalsButton = nullptr;
 	wxButton* clearButton = nullptr;
 
@@ -28,10 +46,27 @@ public:
 
 
 	Window();
+	void ZeroClicked(wxCommandEvent& zero);
+	void OneClicked(wxCommandEvent& one);
+	void TwoClicked(wxCommandEvent& two);
+	void ThreeClicked(wxCommandEvent& three);
+	void FourClicked(wxCommandEvent& four);
+	void FiveClicked(wxCommandEvent& five);
+	void SixClicked(wxCommandEvent& six);
+	void SevenClicked(wxCommandEvent& seven);
+	void EightClicked(wxCommandEvent& eight);
+	void NineClicked(wxCommandEvent& nine);
 	void AddClicked(wxCommandEvent& add);
 	void SubtractClicked(wxCommandEvent& subtract);
 	void MultiplyClicked(wxCommandEvent& multiply);
 	void DivideClicked(wxCommandEvent& divide);
+
+
+
+
+
+
+
 	void EqualsClicked(wxCommandEvent& equals);
 	void ClearClicked(wxCommandEvent& clear);
 	
