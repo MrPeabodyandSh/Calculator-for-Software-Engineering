1361dcf9077a42b5feb270855a125f205415f43f
Xavier Baez <XEBaez@student.fullsail.edu>
2024-07-14 23:03:14 -0400

    All button functionality implemented

--- Calculator for Software Engineering/Window.h
+++ Calculator for Software Engineering/Window.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "wx/wx.h"
+#include <wx/tokenzr.h>
+#include <algorithm>
 
 class Window : public wxFrame
 {
@@ -17,30 +19,31 @@ class Window : public wxFrame
 	wxButton* subtractButton = nullptr;
 	wxButton* multiplyButton = nullptr;
 	wxButton* divideButton = nullptr;
-
 	wxButton* moduloButton = nullptr;
 	wxButton* sinButton = nullptr;
-	wxButton* cosButton = nullptr;             //Implement
-	wxButton* tanButton = nullptr;            //Implement
-	wxButton* backspaceButton = nullptr;         //Implement
+	wxButton* cosButton = nullptr;
+	wxButton* tanButton = nullptr;
+	wxButton* backspaceButton = nullptr;
 	wxButton* decimalButton = nullptr;
 	wxButton* negativeButton = nullptr;
-
 	wxButton* equalsButton = nullptr;
 	wxButton* clearButton = nullptr;
 
-	wxTextCtrl* firstNumber = nullptr;
-	wxTextCtrl* secondNumber = nullptr;
+	wxTextCtrl* equation = nullptr;
 	wxTextCtrl* answer = nullptr;
 
 	wxString FirstNumber;
 	wxString SecondNumber;
 
-	float Answer = 0;
+	double Answer = 0;
 	bool addClick = false;
 	bool subtractClick = false;
 	bool multiplyClick = false;
 	bool divideClick = false;
+	bool moduloClick = false;
+	bool sinClick = false;
+	bool cosClick = false;
+	bool tanClick = false;
 
 public:
 
@@ -60,17 +63,16 @@ public:
 	void SubtractClicked(wxCommandEvent& subtract);
 	void MultiplyClicked(wxCommandEvent& multiply);
 	void DivideClicked(wxCommandEvent& divide);
-
-
-
-
-
-
-
+	void ModuloClicked(wxCommandEvent& modulo);
+	void SinClicked(wxCommandEvent& Sin);
+	void CosClicked(wxCommandEvent& Cos);
+	void TanClicked(wxCommandEvent& Tan);
+	void BackspaceClicked(wxCommandEvent& backspace);
+	void DecimalClicked(wxCommandEvent& decimal);
+	void NegativeClicked(wxCommandEvent& negative);
 	void EqualsClicked(wxCommandEvent& equals);
 	void ClearClicked(wxCommandEvent& clear);
-	
-	
+
 	wxDECLARE_EVENT_TABLE();
 };
 
